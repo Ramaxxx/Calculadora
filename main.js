2c04ccf37ce913b1e1f3621951d4e7f8451d5f93
Ramiro <ramirosales52@gmail.com>
2022-09-21 20:10:59 -0300

    funcionando con 2 numeros

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-var numero = 0
-var numero1 = 0
-var operador = 0
+var numero1 = null
+var numero2 = null
+var operador = ''
+var bandera = false
 
 const display = document.querySelector('#display')
 
@@ -17,32 +18,70 @@ const e = Math.E
 
 boton_numero.forEach(btn =>{
     btn.addEventListener('click', () =>{
-        display.innerText += btn.innerText
+        if (bandera === true){
+            display.innerText = ''
+            bandera = false
+            display.innerText += btn.innerText
+        }else{
+            display.innerText += btn.innerText
+        }
     })
 })
 
 boton_operacion.forEach(btn =>{
     btn.addEventListener('click', () =>{
+        operador = ''
+        numero1 = parseInt(display.innerText)
+        bandera = true
+        display.innerText = ''
         display.innerText += btn.innerText
 
         if ((btn).innerText === '+'){
-            console.log("suma")
+            operador = '+'
         }
         else if ((btn).innerText === '-'){
-            console.log("resta")
+            operador = '-'
         }
         else if ((btn).innerText === '/'){
-            console.log("div")
+            operador = '/'
         }
         else if ((btn).innerText === '*'){
-            console.log("mult")
+            operador = '*'
         }
 
     })
 })
 
+function operar(operador, num1, num2){
+    if (operador === '+'){
+        return num1 + num2
+    }
+    else if (operador === '-'){
+        return num2 - num1
+    }
+    else if (operador === '/'){
+        return num1 / num2
+    }
+    else{
+        return num1 * num2
+    }
+}
+
+boton_igual.addEventListener('click', () =>{
+    numero2 = parseInt(display.innerText)
+    display.innerText = operar(operador, numero1, numero2)
+    operador = ''
+    numero1 = null
+    numero2 = null
+    bandera = false
+})
+
 boton_borrar.addEventListener('click', () =>{
     display.innerText = ''
+    operador = ''
+    numero1 = null
+    numero2 = null
+    bandera = false
 })
 
 boton_pi.addEventListener('click', () =>{
